90e3932c72f05a98185cfb5b8be1827907ccd6b6
malik houni <malik.houni@gmail.com>
2023-05-02 00:39:20 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -74,54 +74,6 @@ data_dict = {
 }
  
 
-# load the model
-final_nb_model = load(open('/content/drive/MyDrive/ColabNotebooks/Flask_Disease_Prediction/final_nb_model.pkl', 'rb'))
-# load the model
-final_rf_model = load(open('/content/drive/MyDrive/ColabNotebooks/Flask_Disease_Prediction/final_rf_model.pkl', 'rb'))
-# load the model
-final_svm_model = load(open('/content/drive/MyDrive/ColabNotebooks/Flask_Disease_Prediction/final_svm_model.pkl', 'rb'))
-
-
-# Defining the Function
-# Input: string containing symptoms separated by commas
-# Output: Generated predictions by models
-def predictDisease(symptoms):
-    #symptoms = symptoms.split(",")
-     
-    # creating input data for the models
-    input_data = [0] * len(data_dict["symptom_index"])
-    for symptom in symptoms:
-        index = data_dict["symptom_index"][symptom]
-        input_data[index] = 1
-         
-    # reshaping the input data and converting it
-    # into suitable format for model predictions
-    input_data = np.array(input_data).reshape(1,-1)
-     
-    # generating individual outputs
-    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
-    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
-    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
-     
-    # making final prediction by taking mode of all predictions
-    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
-    predictions = {
-        "rf_model_prediction": rf_prediction,
-        "naive_bayes_prediction": nb_prediction,
-        "svm_model_prediction": svm_prediction,
-        "final_prediction":final_prediction
-    }
-    return predictions
- 
-def removeStupidSpaceInJson(currArray):
-    newArray=[]
-    for i in range(0,len(currArray)):
-        currElem=currArray[i].lstrip(currArray[i][0])#remove first elem in string
-        currElem=currElem[:-1]#remove last elem in string
-        newArray.append(currElem.replace("_", " " ).title())#.title() put into majuscule the first letter
-    return newArray
-
-
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__, template_folder='/')
 run_with_ngrok(app)   
@@ -129,11 +81,3 @@ run_with_ngrok(app)
 @app.route("/")
 def home():
     return render_template('index.html',data=["toto","tata"])
-
-@app.route("/ASimpleRoute" ,methods=["POST"])
-def getMyRecommendation():
-  if request.method == 'POST':
-      data = request.json
-      resDiseasePredicted=predictDisease(removeStupidSpaceInJson(data)).get('final_prediction')
-      return jsonify(resDiseasePredicted)
-  return render_template("index.html")
