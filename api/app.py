b501ae868aae5127017755e49f310cc620d9c188
malik houni <malik.houni@gmail.com>
2023-05-02 00:48:19 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,15 +1,7 @@
-import numpy as np
-import pandas as pd
-# Importing libraries
+from flask import Flask, render_template
 
-# Creating a symptom index dictionary to encode the
-# input symptoms into numerical form
- 
+app = Flask(__name__, template_folder='./')
 
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__, template_folder='/')
-run_with_ngrok(app)   
-  
 @app.route("/")
-def home():
-    return render_template('index.html',data=["toto","tata"])
+def hello_world():
+    return render_template('index.html')
