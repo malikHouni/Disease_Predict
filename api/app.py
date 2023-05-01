a9c52a7edabcaa95733157006121de6ba1265142
malik houni <malik.houni@gmail.com>
2023-05-01 18:36:45 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,12 +2,6 @@ import numpy as np
 import pandas as pd
 # Importing libraries
 from scipy.stats import mode
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
 from pickle import dump, load
 import warnings
 warnings.filterwarnings("ignore")
