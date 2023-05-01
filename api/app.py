b4508e2266b8b6b53b01cd740d25efdfe8c4aac7
malik houni <malik.houni@gmail.com>
2023-05-02 00:44:35 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,23 +1,9 @@
 import numpy as np
 import pandas as pd
 # Importing libraries
-from scipy.stats import mode
-from pickle import dump, load
-import warnings
-warnings.filterwarnings("ignore")
-
 
 # Creating a symptom index dictionary to encode the
 # input symptoms into numerical form
-symptom_index = {}
-for index, value in enumerate(symptoms):
-    symptom = " ".join([i.capitalize() for i in value.split("_")])
-    symptom_index[symptom] = index
- 
-data_dict = {
-    "symptom_index":symptom_index,
-    "predictions_classes":encoder_classes_
-}
  
 
 from flask import Flask, render_template, request, jsonify
