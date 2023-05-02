f932b503220c83b64150a0ed73c3e87cfa0f1ce5
malik houni <malik.houni@gmail.com>
2023-05-02 11:37:20 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,146 @@
-from flask import Flask
+import numpy as np
+import pandas as pd
+# Importing libraries
+from scipy.stats import mode
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from pickle import dump, load
+import warnings
+warnings.filterwarnings("ignore")
 
-app = Flask(__name__)
+symptoms = ['itching', 'skin_rash', 'nodal_skin_eruptions',
+       'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
+       'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting',
+       'vomiting', 'burning_micturition', 'spotting_ urination',
+       'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets',
+       'mood_swings', 'weight_loss', 'restlessness', 'lethargy',
+       'patches_in_throat', 'irregular_sugar_level', 'cough',
+       'high_fever', 'sunken_eyes', 'breathlessness', 'sweating',
+       'dehydration', 'indigestion', 'headache', 'yellowish_skin',
+       'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes',
+       'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea',
+       'mild_fever', 'yellow_urine', 'yellowing_of_eyes',
+       'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach',
+       'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision',
+       'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure',
+       'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs',
+       'fast_heart_rate', 'pain_during_bowel_movements',
+       'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus',
+       'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity',
+       'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes',
+       'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties',
+       'excessive_hunger', 'extra_marital_contacts',
+       'drying_and_tingling_lips', 'slurred_speech', 'knee_pain',
+       'hip_joint_pain', 'muscle_weakness', 'stiff_neck',
+       'swelling_joints', 'movement_stiffness', 'spinning_movements',
+       'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side',
+       'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine',
+       'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching',
+       'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain',
+       'altered_sensorium', 'red_spots_over_body', 'belly_pain',
+       'abnormal_menstruation', 'dischromic _patches',
+       'watering_from_eyes', 'increased_appetite', 'polyuria',
+       'family_history', 'mucoid_sputum', 'rusty_sputum',
+       'lack_of_concentration', 'visual_disturbances',
+       'receiving_blood_transfusion', 'receiving_unsterile_injections',
+       'coma', 'stomach_bleeding', 'distention_of_abdomen',
+       'history_of_alcohol_consumption', 'fluid_overload.1',
+       'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations',
+       'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring',
+       'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails',
+       'inflammatory_nails', 'blister', 'red_sore_around_nose',
+       'yellow_crust_ooze']
+encoder_classes_=['(vertigo) Paroymsal  Positional Vertigo', 'AIDS', 'Acne',
+       'Alcoholic hepatitis', 'Allergy', 'Arthritis', 'Bronchial Asthma',
+       'Cervical spondylosis', 'Chicken pox', 'Chronic cholestasis',
+       'Common Cold', 'Dengue', 'Diabetes ',
+       'Dimorphic hemmorhoids(piles)', 'Drug Reaction',
+       'Fungal infection', 'GERD', 'Gastroenteritis', 'Heart attack',
+       'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E',
+       'Hypertension ', 'Hyperthyroidism', 'Hypoglycemia',
+       'Hypothyroidism', 'Impetigo', 'Jaundice', 'Malaria', 'Migraine',
+       'Osteoarthristis', 'Paralysis (brain hemorrhage)',
+       'Peptic ulcer diseae', 'Pneumonia', 'Psoriasis', 'Tuberculosis',
+       'Typhoid', 'Urinary tract infection', 'Varicose veins',
+       'hepatitis A']
+# Creating a symptom index dictionary to encode the
+# input symptoms into numerical form
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+ 
+data_dict = {
+    "symptom_index":symptom_index,
+    "predictions_classes":encoder_classes_
+}
+ 
 
-@app.route('/')
+# load the model
+final_nb_model = load(open('../final_nb_model.pkl', 'rb'))
+# load the model
+final_rf_model = load(open('../final_rf_model.pkl', 'rb'))
+# load the model
+final_svm_model = load(open('../final_svm_model.pkl', 'rb'))
+
+
+# Defining the Function
+# Input: string containing symptoms separated by commas
+# Output: Generated predictions by models
+def predictDisease(symptoms):
+    #symptoms = symptoms.split(",")
+     
+    # creating input data for the models
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom]
+        input_data[index] = 1
+         
+    # reshaping the input data and converting it
+    # into suitable format for model predictions
+    input_data = np.array(input_data).reshape(1,-1)
+     
+    # generating individual outputs
+    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
+    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
+     
+    # making final prediction by taking mode of all predictions
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction":final_prediction
+    }
+    return predictions
+ 
+def removeStupidSpaceInJson(currArray):
+    newArray=[]
+    for i in range(0,len(currArray)):
+        currElem=currArray[i].lstrip(currArray[i][0])#remove first elem in string
+        currElem=currElem[:-1]#remove last elem in string
+        newArray.append(currElem.replace("_", " " ).title())#.title() put into majuscule the first letter
+    return newArray
+
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__, template_folder='./')
+run_with_ngrok(app)   
+  
+@app.route("/")
 def home():
-    return 'Hello, Maliko yo!'
+    return render_template('index.html',data=symptoms)
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/ASimpleRoute" ,methods=["POST"])
+def getMyRecommendation():
+  if request.method == 'POST':
+      data = request.json
+      resDiseasePredicted=predictDisease(removeStupidSpaceInJson(data)).get('final_prediction')
+      return jsonify(resDiseasePredicted)
+  return render_template("index.html")
+
